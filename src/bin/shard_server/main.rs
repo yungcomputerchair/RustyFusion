41062c76b6ff9589a20d26acc3466b5930792e75
gsemaj <gsemaj@proton.me>
2023-08-30 12:43:29 -0700

    login module on shard

--- src/bin/shard_server/main.rs
+++ src/bin/shard_server/main.rs
@@ -28,7 +28,7 @@ fn main() -> Result<()> {
 
     let ls_addr: &str = "127.0.0.1:23000";
     let ls: &mut CNClient = server.connect(ls_addr, ClientType::LoginServer);
-    login_server_connect_req(ls);
+    login::login_connect_req(ls);
     thread::sleep(Duration::from_millis(2000));
     server.poll(&handle_packet)?;
     verify_login_server_conn();
@@ -47,8 +47,9 @@ fn handle_packet(
     let client: &mut CNClient = clients.get_mut(key).unwrap();
     println!("{} sent {:?}", client.get_addr(), pkt_id);
     match pkt_id {
-        P_LS2FE_REP_CONNECT_SUCC => login_server_connect_succ(client),
-        P_LS2FE_REP_CONNECT_FAIL => login_server_connect_fail(client),
+        P_LS2FE_REP_CONNECT_SUCC => login::login_connect_succ(client),
+        P_LS2FE_REP_CONNECT_FAIL => login::login_connect_fail(client),
+        P_LS2FE_REQ_UPDATE_LOGIN_INFO => login::login_update_info(client),
         //
         P_CL2LS_REQ_LOGIN => wrong_server(client),
         other => {
@@ -69,37 +70,48 @@ fn wrong_server(client: &mut CNClient) -> Result<()> {
     Ok(())
 }
 
-fn login_server_connect_req(server: &mut CNClient) {
-    let pkt = sP_FE2LS_REQ_CONNECT { iTempValue: 0 };
-    server
-        .send_packet(P_FE2LS_REQ_CONNECT, &pkt)
-        .expect("Couldn't connect to login server");
-}
-
-fn login_server_connect_succ(server: &mut CNClient) -> Result<()> {
-    let pkt: &sP_LS2FE_REP_CONNECT_SUCC = server.get_packet();
-    let conn_id: i64 = pkt.iConn_UID;
-    let conn_time: u64 = pkt.uiSvrTime;
-
-    let iv1: i32 = (conn_id + 1) as i32;
-    let iv2: i32 = 69;
-    server.set_e_key(gen_key(conn_time, iv1, iv2));
-
-    LOGIN_SERVER_CONN_ID.store(conn_id, Ordering::Relaxed);
-    println!("Connected to login server ({})", server.get_addr());
-    Ok(())
-}
-
-fn login_server_connect_fail(server: &mut CNClient) -> Result<()> {
-    let pkt: &sP_LS2FE_REP_CONNECT_FAIL = server.get_packet();
-    panic!("Login server refused to connect (error {})", {
-        pkt.iErrorCode
-    });
-}
-
 fn verify_login_server_conn() {
     let conn_id: i64 = LOGIN_SERVER_CONN_ID.load(Ordering::Relaxed);
     if conn_id == CONN_ID_DISCONNECTED {
         panic!("Couldn't handshake with login server in time");
+    }
+}
+
+mod login {
+    use super::*;
+
+    pub fn login_connect_req(server: &mut CNClient) {
+        let pkt = sP_FE2LS_REQ_CONNECT { iTempValue: 0 };
+        server
+            .send_packet(P_FE2LS_REQ_CONNECT, &pkt)
+            .expect("Couldn't connect to login server");
+    }
+
+    pub fn login_connect_succ(server: &mut CNClient) -> Result<()> {
+        let pkt: &sP_LS2FE_REP_CONNECT_SUCC = server.get_packet();
+        let conn_id: i64 = pkt.iConn_UID;
+        let conn_time: u64 = pkt.uiSvrTime;
+
+        let iv1: i32 = (conn_id + 1) as i32;
+        let iv2: i32 = 69;
+        server.set_e_key(gen_key(conn_time, iv1, iv2));
+
+        LOGIN_SERVER_CONN_ID.store(conn_id, Ordering::Relaxed);
+        println!("Connected to login server ({})", server.get_addr());
+        Ok(())
+    }
+
+    pub fn login_connect_fail(server: &mut CNClient) -> Result<()> {
+        let pkt: &sP_LS2FE_REP_CONNECT_FAIL = server.get_packet();
+        panic!("Login server refused to connect (error {})", {
+            pkt.iErrorCode
+        });
+    }
+
+    pub fn login_update_info(server: &mut CNClient) -> Result<()> {
+        let pkt: &sP_LS2FE_REP_CONNECT_FAIL = server.get_packet();
+        panic!("Login server refused to connect (error {})", {
+            pkt.iErrorCode
+        });
     }
 }
