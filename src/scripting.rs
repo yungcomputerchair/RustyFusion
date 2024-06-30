b3ac7c24d8c1222ce6c25d990eca8f85a5ec4d1a
gsemaj <gsemaj@proton.me>
2024-06-29 18:51:16 -0700

    Enable loading of scripts not attached to an entity

--- src/scripting.rs
+++ src/scripting.rs
@@ -11,10 +11,11 @@ use crate::{
 
 pub struct ScriptManager {
     lua: Lua,
-    registered_entities: HashMap<EntityID, LuaRegistryKey>,
+    global_env: LuaRegistryKey,
+    entity_envs: HashMap<EntityID, LuaRegistryKey>,
 }
 impl ScriptManager {
-    fn new_internal() -> LuaResult<Lua> {
+    fn new_internal() -> LuaResult<(Lua, LuaRegistryKey)> {
         let lua = Lua::new();
 
         // Redirect Lua print() to our log() function
@@ -36,81 +37,90 @@ impl ScriptManager {
         let blackboard = lua.create_table()?;
         lua.globals().set("bb", blackboard)?;
 
+        // Environment for non-entity scripts
+        let global_env = lua.create_table()?;
+        let global_env_key = lua.create_registry_value(global_env)?;
+
         lua.load("print('Scripting engine initialized')").exec()?;
 
-        Ok(lua)
+        Ok((lua, global_env_key))
     }
 
     pub fn new() -> FFResult<Self> {
         Self::new_internal()
-            .map(|lua| Self {
+            .map(|(lua, global_env_key)| Self {
                 lua,
-                registered_entities: HashMap::new(),
+                global_env: global_env_key,
+                entity_envs: HashMap::new(),
             })
             .map_err(FFError::from_lua_err)
     }
 
-    fn load_script_for_entity_internal(
+    fn load_script_internal(
         &mut self,
-        entity_id: EntityID,
+        entity_id: Option<EntityID>,
         source: &str,
         name: Option<&str>,
     ) -> LuaResult<()> {
+        let script_scope = match entity_id {
+            Some(eid) => eid.to_string(),
+            None => "Global".to_string(),
+        };
         let namespace = match name {
-            Some(name) => format!("{}:{}", entity_id, name),
-            None => format!("{}:<raw>", entity_id),
+            Some(name) => format!("{}:{}", script_scope, name),
+            None => format!("{}:<raw>", script_scope),
         };
         let chunk = self.lua.load(source).set_name(namespace);
 
-        // populate the entity environment if it doesn't exist
-        if !self.is_entity_registered(entity_id) {
-            let entity_env_key = self.make_entity_env(entity_id)?;
-            self.registered_entities.insert(entity_id, entity_env_key);
-        }
-        let entity_env_key = self.registered_entities.get(&entity_id).unwrap();
+        // populate the environment
+        let env_key = match entity_id {
+            Some(eid) => {
+                // populate the entity environment if it doesn't exist
+                if !self.is_entity_registered(eid) {
+                    let entity_env_key = self.make_env()?;
+                    let entity_env = self
+                        .lua
+                        .registry_value::<LuaTable>(&entity_env_key)
+                        .unwrap();
+                    entity_env.set("entity_id", eid.to_string())?;
+                    self.entity_envs.insert(eid, entity_env_key);
+                }
+                self.entity_envs.get(&eid).unwrap()
+            }
+            None => &self.global_env,
+        };
 
-        let env = self.lua.registry_value::<LuaTable>(entity_env_key)?;
+        let env = self.lua.registry_value::<LuaTable>(env_key)?;
         chunk.set_environment(env).exec()?;
         Ok(())
     }
 
     fn is_entity_registered(&self, entity_id: EntityID) -> bool {
-        self.registered_entities.contains_key(&entity_id)
+        self.entity_envs.contains_key(&entity_id)
     }
 
-    fn make_entity_env(&self, entity_id: EntityID) -> LuaResult<LuaRegistryKey> {
-        let entity_env = self.lua.create_table()?;
-
-        // pre-populate some useful values
-        entity_env.set("entity_id", entity_id.to_string())?;
+    fn make_env(&self) -> LuaResult<LuaRegistryKey> {
+        let env = self.lua.create_table()?;
 
         // link allowed Lua globals
         let aliases = ["bb", "print"];
         for &alias in aliases.iter() {
-            entity_env.set(alias, self.lua.globals().get::<_, LuaValue>(alias)?)?;
+            env.set(alias, self.lua.globals().get::<_, LuaValue>(alias)?)?;
         }
 
-        let entity_env_key = self.lua.create_registry_value(entity_env)?;
-        Ok(entity_env_key)
+        let env_key = self.lua.create_registry_value(env)?;
+        Ok(env_key)
     }
 
-    pub fn load_script_for_entity(
-        &mut self,
-        entity_id: EntityID,
-        script_name: &str,
-    ) -> FFResult<()> {
+    pub fn load_script(&mut self, entity_id: Option<EntityID>, script_name: &str) -> FFResult<()> {
         let path = format!("scripts/{}.lua", script_name);
         let source = util::get_text_file_contents(&path)?;
-        self.load_script_for_entity_internal(entity_id, &source, Some(script_name))
+        self.load_script_internal(entity_id, &source, Some(script_name))
             .map_err(FFError::from_lua_err)
     }
 
-    pub fn load_script_for_entity_raw(
-        &mut self,
-        entity_id: EntityID,
-        source: &str,
-    ) -> FFResult<()> {
-        self.load_script_for_entity_internal(entity_id, source, None)
+    pub fn load_script_raw(&mut self, entity_id: Option<EntityID>, source: &str) -> FFResult<()> {
+        self.load_script_internal(entity_id, source, None)
             .map_err(FFError::from_lua_err)
     }
 
@@ -119,7 +129,7 @@ impl ScriptManager {
         entity_id: EntityID,
         _state: &mut ShardServerState,
     ) -> LuaResult<()> {
-        let entity_env_key = self.registered_entities.get(&entity_id).unwrap();
+        let entity_env_key = self.entity_envs.get(&entity_id).unwrap();
         let entity_env = self.lua.registry_value::<LuaTable>(entity_env_key)?;
         let tick_fn = entity_env.get::<_, LuaFunction>("tick")?;
         tick_fn.set_environment(entity_env.clone())?;
@@ -128,7 +138,7 @@ impl ScriptManager {
     }
 
     pub fn tick_entity_scripts(&mut self, state: &mut ShardServerState) -> FFResult<()> {
-        let to_tick = self.registered_entities.keys().cloned().collect::<Vec<_>>();
+        let to_tick = self.entity_envs.keys().cloned().collect::<Vec<_>>();
         for eid in to_tick {
             match self.tick_entity_script(eid, state) {
                 Ok(_) => {}
@@ -137,7 +147,7 @@ impl ScriptManager {
                         Severity::Warning,
                         &format!("Error ticking script for entity {:?}: {}", eid, e),
                     );
-                    self.registered_entities.remove(&eid);
+                    self.entity_envs.remove(&eid);
                 }
             }
         }
