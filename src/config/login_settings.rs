3ed98e424170337b4712dc3423558cdf1fe7676c
Gent Semaj <me@ycc.dev>
2025-11-28 17:14:28 -0800

    Add config option to allow plaintext password auth

--- src/config/login_settings.rs
+++ src/config/login_settings.rs
@@ -4,6 +4,7 @@ use crate::defines::CN_ACCOUNT_LEVEL__USER;
 
 define_setting!(LogPathSetting, String, "login.log");
 define_setting!(ListenAddrSetting, String, "127.0.0.1:23000");
+define_setting!(AllowPlaintextPasswordsSetting, bool, false);
 define_setting!(AutoCreateAccountsSetting, bool, true);
 define_setting!(DefaultAccountLevelSetting, u32, CN_ACCOUNT_LEVEL__USER);
 define_setting!(AutoApproveCustomNamesSetting, bool, true);
@@ -16,6 +17,7 @@ define_setting!(MonitorIntervalSetting, u64, 5_u64);
 pub struct LoginConfig {
     pub log_path: LogPathSetting,
     pub listen_addr: ListenAddrSetting,
+    pub allow_plaintext_passwords: AllowPlaintextPasswordsSetting,
     pub auto_create_accounts: AutoCreateAccountsSetting,
     pub default_account_level: DefaultAccountLevelSetting,
     pub auto_approve_custom_names: AutoApproveCustomNamesSetting,
@@ -35,6 +37,7 @@ mod tests {
         let config = Config::load("config.toml.default").unwrap();
         let login = config.login;
         assert!(login.log_path.is_set_to_default());
+        assert!(login.allow_plaintext_passwords.is_set_to_default());
         assert!(login.listen_addr.is_set_to_default());
         assert!(login.auto_create_accounts.is_set_to_default());
         assert!(login.default_account_level.is_set_to_default());
