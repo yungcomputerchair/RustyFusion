314594a9006306c40e9abfee7658fd3ea6452740
Gent Semaj <me@ycc.dev>
2026-03-12 14:12:51 -0700

    Add geo_db_path login server setting

--- src/config/login_settings.rs
+++ src/config/login_settings.rs
@@ -4,6 +4,7 @@ use crate::defines::CN_ACCOUNT_LEVEL__USER;
 
 define_setting!(LogPathSetting, String, "login.log");
 define_setting!(ListenAddrSetting, String, "127.0.0.1:23000");
+define_setting!(GeoDbPathSetting, String, "GeoLite2-City.mmdb");
 define_setting!(AllowPlaintextPasswordsSetting, bool, false);
 define_setting!(AutoCreateAccountsSetting, bool, true);
 define_setting!(DefaultAccountLevelSetting, u32, CN_ACCOUNT_LEVEL__USER);
@@ -17,6 +18,7 @@ define_setting!(MonitorIntervalSetting, u64, 5_u64);
 pub struct LoginConfig {
     pub log_path: LogPathSetting,
     pub listen_addr: ListenAddrSetting,
+    pub geo_db_path: GeoDbPathSetting,
     pub allow_plaintext_passwords: AllowPlaintextPasswordsSetting,
     pub auto_create_accounts: AutoCreateAccountsSetting,
     pub default_account_level: DefaultAccountLevelSetting,
@@ -37,8 +39,9 @@ mod tests {
         let config = Config::load("config.toml.default").unwrap();
         let login = config.login;
         assert!(login.log_path.is_set_to_default());
-        assert!(login.allow_plaintext_passwords.is_set_to_default());
         assert!(login.listen_addr.is_set_to_default());
+        assert!(login.geo_db_path.is_set_to_default());
+        assert!(login.allow_plaintext_passwords.is_set_to_default());
         assert!(login.auto_create_accounts.is_set_to_default());
         assert!(login.default_account_level.is_set_to_default());
         assert!(login.auto_approve_custom_names.is_set_to_default());
