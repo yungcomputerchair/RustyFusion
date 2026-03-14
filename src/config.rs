cc4cd7875a202d9c65b277c1719a7c400e9d9520
Gent Semaj <me@ycc.dev>
2026-03-13 23:04:31 -0700

    Automate section codegen as well

--- src/config.rs
+++ src/config.rs
@@ -9,46 +9,6 @@ include!(concat!(env!("OUT_DIR"), "/config_generated.rs"));
 static CONFIG: OnceLock<Config> = OnceLock::new();
 static CONFIG_DEFAULT: OnceLock<Config> = OnceLock::new();
 
-#[derive(Default)]
-pub struct Config {
-    pub general: GeneralConfig,
-    pub login: LoginConfig,
-    pub shard: ShardConfig,
-}
-impl Config {
-    fn load(path: &str) -> Option<Self> {
-        #[derive(Deserialize)]
-        struct ConfigLayout {
-            general: Option<GeneralConfig>,
-            login: Option<LoginConfig>,
-            shard: Option<ShardConfig>,
-        }
-        let file_read = std::fs::read_to_string(path);
-        if let Err(e) = file_read {
-            if let std::io::ErrorKind::NotFound = e.kind() {
-                log(
-                    Severity::Warning,
-                    &format!("Config file {} missing, using default config", path),
-                );
-                return None;
-            } else {
-                panic_log(&format!("Can't open config file {}: {}", path, e));
-            }
-        }
-
-        let file_contents = file_read.unwrap();
-        let parsed: ConfigLayout = toml::from_str(&file_contents).unwrap_or_else(|e| {
-            panic_log(&format!("Malformed config file: {}", e));
-        });
-
-        Some(Config {
-            general: parsed.general.unwrap_or_default(),
-            login: parsed.login.unwrap_or_default(),
-            shard: parsed.shard.unwrap_or_default(),
-        })
-    }
-}
-
 pub fn config_init() -> &'static Config {
     assert!(CONFIG.get().is_none());
 
